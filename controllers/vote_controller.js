1b138ce1c5bbb17a1ce90f96d2eba0d12a3d2cba
mhneri <mh.neri@gmail.com>
2017-03-26 19:17:12 +0800

    added catch handler. added vote direction to route

--- controllers/vote_controller.js
+++ controllers/vote_controller.js
@@ -3,15 +3,19 @@ const models = require('../models');
 class VoteController {
   static createByThreadId(req, res) {
     const threadId = req.params.thread_id;
+    const direction = req.params.direction;
 
     models.Vote
       .build({
-        direction: 'up',
+        direction,
         vote_to: threadId,
       })
       .save()
       .then(() => {
         res.send({ result: 'ok' });
+      })
+      .catch(() => {
+        res.send({ error: true });
       });
   }
 }
--- routers/vote_router.js
+++ routers/vote_router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const VoteController = require('../controllers/vote_controller');
 
-router.get('/addto/:thread_id', VoteController.createByThreadId);
+router.get('/addto/:thread_id/:direction', VoteController.createByThreadId);
 
 module.exports = router;
