6955944ff92fbda0a46517f19d0436acebbdb491
mhneri <mh.neri@gmail.com>
2017-03-26 15:52:07 +0800

    removed view bits

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'pug');
-
 const {VoteController} = require('./controllers/vote_controller');
 
 app.get('/api/threads/:thread_id/votes', VoteController.createByThreadId);
